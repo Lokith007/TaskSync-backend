1f00a46877f7ace546e95ea6a5a6db7cdbe40bae
Pranatheesh-S <pranatheesh@gmail.com>
2025-05-23 20:01:20 +0530

    changes in db

--- models/task.js
+++ models/task.js
@@ -1,27 +1,48 @@
 const mongoose=require("mongoose");
 
-const taskSchema=new mongoose.Schema({
-    header:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String
-    },
-    dueDate:{
-        type:Date,
-        required:true
-    },
-    priorityLevel:{
-        type:String,
-        enum: ['low', 'medium', 'high'],
-        required:true
-    },
-    completed:{
-        type:Boolean,
-        default:false
-    }
+const connectDB=async()=>{
+    try {
+        await mongoose.connect("mongodb://localhost:27017/TaskSync");
+        console.log("Database connected");
+    }catch(err){
+        console.log("Connection failed:"+err);
+    }    
+};
+
+const taskSchema = new mongoose.Schema({
+  header: {
+    type: String,
+    required: true
+  },
+  description: String,
+  dueDate: {
+    type: Date,
+    required: true
+  },
+  priorityLevel: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  subscription :{
+    type:String
+  },
+  assignedTo: {
+    type: String,   // references userId
+    required: true
+  },
+  organization: {
+    type: String,   // references organizationId
+    required: true
+  },
+  completedAt: { 
+    type:Date
+}
 });
 
-const task=mongoose.model("Tasks","TaskSync");
-modules.export(task);
+const task=mongoose.model("Tasks",taskSchema);
+modules.export=task;
